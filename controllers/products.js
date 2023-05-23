2fe7447202c6cb245d6f0dd4d894381270e7e12c
DRAGOS BALUTA <DIULIANB16@GMAIL.COM>
2023-05-23 22:14:59 +0300

    -filter functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,11 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "vase table",
-  });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
 
@@ -17,8 +15,30 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
